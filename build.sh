603370d7f00f6f6de887a9fabc799f59664fcb1a
Tomasz Ciborski <tilosag@gmail.com>
2021-01-22 05:43:48 +0100

    wip

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
 VERSION=`cat VERSION`
-TAG="samba-${VERSION}"
 
-docker build --build-arg "TAG=${TAG}" -t "episage/samba:${VERSION}" .
+docker build --build-arg "TAG=${VERSION}" -t "episage/samba:${VERSION}" .
